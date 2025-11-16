f452b618c34cbcf1f1571f1a542ff2e48d9b5650
Your Name <your-email@example.com>
2025-11-16 21:52:03 +0530

    Refactored data type examples and added comments for clarity

--- DataTypes.py
+++ DataTypes.py
@@ -2,7 +2,18 @@ a= 12
 b="Aditya"
 c=12.5
 d=True
-print(type(a))
-print(type(b))
-print(type(c))
-print(type(d))
+e=4+3j
+f=[1,2,3,4,5]
+g=(1,2,3,4,5)
+h={1,2,3,4,5}
+i={"name":"Aditya","age":20}
+
+print(type(a)) # Output: <class 'int'> 
+print(type(b)) # Output: <class 'str'> 
+print(type(c)) # Output: <class 'float'> 
+print(type(d)) # Output: <class 'bool'> 
+print(type(e)) # Output: <class 'complex'> 
+print(type(f)) # Output: <class 'list'> 
+print(type(g)) # Output: <class 'tuple'> 
+print(type(h)) # Output: <class 'set'> 
+print(type(i)) # Output: <class 'dict'#
