8a8d41b6c2ed286509c1b01383227a91e6cccdb5
Your Name <your-email@example.com>
2025-11-30 15:44:35 +0530

    complex datatype

---
+++ complex_datatype.py
@@ -0,0 +1,23 @@
+c = 10+3j
+print("The value of c is:",c)
+print("The type of c is:",type(c))  # Output: <class 'complex
+print("Real part of c is:",c.real)  # Output: 10.0
+print("Imaginary part of c is:",c.imag)  # Output: 3.0
+d = complex(5, -7)
+print("The value of d is:",d)
+print("The type of d is:",type(d))  # Output: <class 'complex
+print("Real part of d is:",d.real)  # Output: 5.0
+print("Imaginary part of d is:",d.imag)  # Output: -7.
+# Complex number arithmetic
+sum_cd = c + d
+print("Sum of c and d is:",sum_cd)  # Output: (15-4j)
+diff_cd = c - d 
+print("Difference of c and d is:",diff_cd)  # Output: (5+10j)
+prod_cd = c * d
+print("Product of c and d is:",prod_cd)  # Output: (71-49j)
+div_cd = c / d
+print("Division of c by d is:",div_cd)  # Output: (-0.29+1.43j)
+z=complex(4)
+print("The value of z is:",z)
+y=complex(0,6)
+print("The value of y is:",y)
